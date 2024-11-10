124a73426ff5beb9cf03dab457cd39c9a5d6938d
VenusWhisper <bytewizardry@gmail.com>
2024-11-10 11:50:15 +0530

    Added some folders

--- calculator_app_v1/lib/main.dart
+++ calculator_app_v1/lib/main.dart
@@ -13,6 +13,7 @@ class CalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      
       debugShowCheckedModeBanner: false,
       home: CalculatorView(),
     );
